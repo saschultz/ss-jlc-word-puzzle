6badadc34221ca64ac2cd2d2c3de176a8ef487a3
jin camou and Sara Schultz <jin@>
2017-03-27 15:55:47 -0700

    create conditional statement applying the javascript method length to find strings within in the array that are greater than or equal to three characters.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,13 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
       var userWords = $("#userSentence").val().split(" ");
-      var newArray = userWords;
-      console.log(userWords);
+      var arrayWords = userWords;
+      arrayWords.map(function(arrayWord) {
+
+      if (arrayWord.length >= 3) {
+      console.log(arrayWord);
+      }
   event.preventDefault();
+  });
   });
 });
